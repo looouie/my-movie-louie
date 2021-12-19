0df0583381e12e436759fe412b73f68aa086f423
Louie chan <loyichan2@gmail.com>
2021-12-19 14:36:26 +0100

    add favourite icon and toggle functionality in movie detail page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,5 +1,8 @@
+import { useEffect, Fragment, useState, useCallback, useMemo } from "react";
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { favouriteActions } from "../store/favourite-slice";
+import * as AiIcons from "react-icons/ai";
 
 import classes from "./MovieDetail.module.css";
 
@@ -7,35 +10,77 @@ import { getMovieDetail } from "../lib/api";
 import useHttp from "../hooks/useHttp";
 
 const MovieDetail = () => {
+  const dispatch = useDispatch();
   const params = useParams();
   const { movieId } = params;
+
   let isValid = false;
+  let payload = {};
+
+  const [isFavourite, setIsFavourite] = useState();
+
+  const list = useSelector((state) => state.favourite.list);
+  useEffect(() => {
+    if (list.find((item) => item.id === parseInt(movieId))) {
+      setIsFavourite(true);
+    }
+  }, [movieId]);
 
   const { sendRequest, data, status, error } = useHttp(getMovieDetail);
   useEffect(() => {
     sendRequest(movieId);
   }, [sendRequest, movieId]);
 
-  console.log(data);
   if (!error && status === "completed") {
     isValid = true;
+    payload = {
+      id: data.id,
+      title: data.title,
+      poster_path: data.poster_path,
+    };
   }
 
+  const toggleFavorite = () => {
+    const addToFavouriteHandler = (payload) => {
+      dispatch(favouriteActions.addToFavouriteList(payload));
+    };
+    const removeFromFavouriteHandler = (id) => {
+      dispatch(favouriteActions.removeFavouriteFromList(id));
+    };
+
+    if (isFavourite) {
+      removeFromFavouriteHandler(movieId);
+      setIsFavourite(false);
+    } else {
+      addToFavouriteHandler(payload);
+      setIsFavourite(true);
+    }
+  };
+
   return (
-    <div>
-      <h1>Movie Detail</h1>
-      <h2>{movieId}</h2>
-      {isValid ? <h3>{data.title}</h3> : "nothing"}
-      {isValid ? (
-        // eslint-disable-next-line jsx-a11y/alt-text
-        <img
-          className={classes.backdrop}
-          src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
-        />
-      ) : (
-        "nothing"
-      )}
-    </div>
+    <Fragment>
+      <div>
+        <h1>Movie Detail</h1>
+        {isValid && [
+          <h2>{data.title}</h2>,
+          <img
+            alt="movie_poster"
+            className={classes.backdrop}
+            src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
+          />,
+        ]}
+        <div className={classes.add_favourite} onClick={toggleFavorite}>
+          {isFavourite ? <AiIcons.AiFillHeart /> : <AiIcons.AiOutlineHeart />}
+        </div>
+      </div>
+      {isValid && [
+        <div className={classes.popularity}>
+          <AiIcons.AiFillStar className={classes.star} />
+          <p>Popularity: {data.popularity}</p>
+        </div>,
+        <p className={classes.description}>{data.overview}</p>,
+      ]}
+    </Fragment>
   );
 };
 
