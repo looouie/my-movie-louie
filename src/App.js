0b8824d5003f6007df2e303a2bab9844ed54e832
Louie chan <loyichan2@gmail.com>
2021-12-26 12:07:57 +0100

    create Search folder under pages and re-organize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Favourite from "./pages/Favourite/Favourite";
-import Search from "./pages/Search";
+import Search from "./pages/Search/Search";
 import WatchLater from "./pages/WatchLater";
 import Main from "./pages/Main";
 import MovieDetail from "./pages/MovieDetail/MovieDetail";
